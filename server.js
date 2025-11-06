4861e916ee89f7712755ccb8762f7be7ea59e4fd
tashaxanavsem82 <swgwull@hotmail.com>
2025-11-06 06:21:21 -0600

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Team Collaboration Tool!');
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
